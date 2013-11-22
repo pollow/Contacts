40fe61e314eb1ed8290b92ad231fe338a629cc2f
gnnng <gnnnnng@gmail.com>
2013-11-22 16:40:22 +0800

    add the login authentication

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ module.exports = function(app) {
   // user
   app.post('/login',userPage.login);
   
-
   // index
   app.get('/', indexPage.index);
 
--- routes/userPage.js
+++ routes/userPage.js
@@ -1,5 +1,34 @@
+var request = require('request');
 
+var inspect = require('util').inspect;
 exports.login = function(req, res) {
+  var authData = {
+  	"username": req.body['user'],
+  	"password": req.body['password']
+  };
+
   console.log(req.body);
-  res.send("login success.");
+  request.post('http://login.mstczju.org/plain', {form: authData }, function(err, response, body) {
+
+    var person = JSON.parse(body); // transform the string to json 
+
+  	if (err) {
+  	  console.log(err);
+  	  console.track(err);
+      return ; // does it need something to return ?
+  	}
+
+    // use the responese code to decide
+  	if (response.statusCode == 200) { 
+    		if (person['success']) {
+    			res.send('login success. Welcome you [' + person['name'] + ']' );
+    		} else {
+    			res.send('login failure.');
+    		}
+    } else {
+  		console.log('Requested error');
+      res.send('login failure.')
+  	}
+  });
+  
 }
